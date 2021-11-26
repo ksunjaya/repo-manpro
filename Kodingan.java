1880fca4284f07572a43f8385c76679163a8a873
Kevin Sunjaya <kssunjaya@gmail.com>
2021-11-26 10:52:04 +0700

    Melakukan perubahan pada Kodingan.java Menambahkan File1.txt

--- Kodingan.java
+++ Kodingan.java
@@ -2,12 +2,13 @@ public class Kodingan{
   
   //kerjaan AAY
   public void A(){
-  
+    System.out.println("Halo nama saya AAY!");
+    System.out.println("Saya sudah mengerjakan tugas saya!");
   }
   
   //kerjaan BETTY
   public void B(){
-  
+    
   }
   
   //JANGAN DIMODIF
