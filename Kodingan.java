93c0455e1578a76e8df1c20e37cdf0b430164af1
Kevin Sunjaya <kssunjaya@gmail.com>
2021-11-26 10:24:11 +0700

    Create Kodingan.java

---
+++ Kodingan.java
@@ -0,0 +1,17 @@
+public class Kodingan{
+  
+  //kerjaan AAY
+  public void A(){
+  
+  }
+  
+  //kerjaan BETTY
+  public void B(){
+  
+  }
+  
+  //JANGAN DIMODIF
+  public static void main(String[] args){
+    
+  }
+}
